ecccde30548a265912d0e5201e4e7ed7b032d21c
Todoca by Alejandro Marcano <info@todoca.com.ve>
2022-12-10 20:53:36 -0400

    Code Rub, create Crud for User

---
+++ dist/auth/auth.service.d.ts
@@ -0,0 +1,11 @@
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+export declare class AuthService {
+    private prisma;
+    constructor(prisma: PrismaService);
+    signup(dto: AuthDto): Promise<User>;
+    signin(): {
+        msg: string;
+    };
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+    //save the new user in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    delete user.hash;
+    //return the saved user
+    return user;
+  }
+  signin() {
+    return { msg: 'I have signed in' };
+  }
+}
